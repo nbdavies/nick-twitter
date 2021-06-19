be1cd2a0145bb35e0d24b2b82228bd7516803241
Nick Davies <nbdavies@gmail.com>
2021-06-19 16:42:38 -0500

    Have an App component for overall contents

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -4,23 +4,14 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const App = props => (
+  <div>Hello!</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
